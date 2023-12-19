a8fdb0c72389ace2c384cd6e312c4e5dde1a84e7
CongDuc02 <duc19042002@gmail.com>
2023-12-20 01:39:03 +0700

    b

--- src/routes/UserRouter.js
+++ src/routes/UserRouter.js
@@ -8,11 +8,11 @@ router.post('/sign-up', userController.createUser)
 router.post('/sign-in', userController.loginUser)
 router.post('/log-out', userController.logoutUser)
 router.put('/update-user/:id', userController.updateUser)
-router.delete('/delete-user/:id', authMiddleWare, userController.deleteUser)
-router.get('/getAll', authMiddleWare, userController.getAllUser)
+router.delete('/delete-user/:id', authUserMiddleWare, userController.deleteUser)
+router.get('/getAll', authUserMiddleWare, userController.getAllUser)
 router.get('/get-details/:id', userController.getDetailsUser)
 router.post('/refresh-token', userController.refreshToken)
-router.post('/delete-many', authMiddleWare, userController.deleteMany)
+router.post('/delete-many', authUserMiddleWare, userController.deleteMany)
 
 
 module.exports = router
